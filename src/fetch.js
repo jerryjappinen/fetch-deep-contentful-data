936f7027c1dffea9f820c68fdd5d053015b565db
Jerry Jäppinen <eiskis@gmail.com>
2020-05-31 12:54:45 +0300

    Can send multiple queries at once

--- src/fetch.js
+++ src/fetch.js
@@ -2,9 +2,16 @@ import fetchList from './fetchList'
 import fetchNestedReferences from './fetchNestedReferences'
 import fetchNestedRichText from './fetchNestedRichText'
 
-export default async (client, query) => {
+// FIXME: support multiple queries
+const fetch = async (client, query) => {
   const initialResponse = await fetchList(client, query)
   const withNestedRichText = await fetchNestedRichText(client, initialResponse)
 
   return fetchNestedReferences(client, withNestedRichText)
+}
+
+export default async (client, ...queries) => {
+  return Promise.all(queries.map((query) => {
+    return fetch(client, query)
+  }))
 }
