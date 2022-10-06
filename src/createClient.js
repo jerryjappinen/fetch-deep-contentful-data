24c710754633a03ff920f2a5821f452609ff5a94
Jerry Jäppinen <eiskis@gmail.com>
2022-10-06 20:58:56 +0200

    Accepting space ID with either parameter name

--- src/createClient.js
+++ src/createClient.js
@@ -3,12 +3,13 @@ import { createClient } from 'contentful'
 // Export constructor that can be used elsewhere
 export default ({
   space,
+  spaceId,
   accessToken,
   previewAccessToken,
   environment
 }) => {
   return createClient({
-    space,
+    space: spaceId || space,
     host: previewAccessToken ? 'preview.contentful.com' : undefined,
     environment: environment || 'master',
     accessToken: previewAccessToken || accessToken
